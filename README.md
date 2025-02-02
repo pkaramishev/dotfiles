f84471adfa3d469fc3c4fc70ad639a44ecd40587
Pavel Karamyshev <karamishev.pv@gmail.com>
2025-02-02 14:54:23 +0300

    feat: add installation script

--- README.md
+++ README.md
@@ -2,7 +2,4 @@
 
 ## Installation
 
-1. Install chezmoi `brew install chezmoi`
-2. Install xcode-commandline-tools `xcode-select --install`
-3. Generate Github access token for new machine at https://github.com/settings/tokens
-4. Initialize everything through chezmoi `chezmoi init https://github.com/pkaramishev/dotfiles.git` (use token as a password)
+Just run `sh install.sh`
---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo ""
+echo "🤚 Initializing everything from scratch..."
+
+# Ask user to generate Github access token
+echo "🤚 First of all, go to https://github.com/settings/tokens and generate new access token for this machine"
+read -n 1 -r -s -p $'    Once you''re done, press any key to continue or Ctrl+C to abort...'
+echo ""
+
+# Install Homebrew
+command -v brew >/dev/null 2>&1 || \
+  (echo '🍺  Installing Homebrew' && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
+
+# Install Oh My Zsh
+if [ ! -f ~/.oh-my-zsh/oh-my-zsh.sh ]; then
+  (echo '💰  Installing oh-my-zsh' && yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)")
+fi
+
+# Install XCode tools for Terminal
+if [ ! -f /Library/Developer/CommandLineTools/usr/bin/git ]; then
+  (echo '💰  Installing XCode tools for Terminal' && xcode-select --install)
+fi
+
+# Install chezmoi
+command -v chezmoi >/dev/null 2>&1 || \
+  (echo '👊  Installing chezmoi' && brew install chezmoi)
+
+if [ -d "$HOME/.local/share/chezmoi/.git" ]; then
+  echo "🚸  chezmoi already initialized"
+  echo "    Reinitialize with: 'chezmoi init https://github.com/pkaramishev/dotfiles.git'"
+else
+  echo "🚀  Initialize dotfiles with:"
+  echo "    chezmoi init https://github.com/pkaramishev/dotfiles.git"
+fi
+
+echo ""
+echo "Done."

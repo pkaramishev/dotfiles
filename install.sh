f9426086a92174bbe941f799a08a5016570ea20d
Pavel Karamyshev <karamishev.pv@gmail.com>
2025-02-12 23:38:37 +0300

    fix: update install instruction to use proper version of command line tools since brew fails with the latest one

--- install.sh
+++ install.sh
@@ -13,9 +13,14 @@ command -v brew >/dev/null 2>&1 || \
   (echo '🍺  Installing Homebrew' && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
 
 # Install XCode tools for Terminal
-if [ ! -f /Library/Developer/CommandLineTools/usr/bin/git ]; then
-  (echo '💰  Installing XCode tools for Terminal' && xcode-select --install)
-fi
+# if [ ! -f /Library/Developer/CommandLineTools/usr/bin/git ]; then
+#  (echo '💰  Installing XCode tools for Terminal' && xcode-select --install)
+#fi
+
+# Ask user to install Command Line Tools as the above command doesn't work
+echo "🤚  Next, go to https://developer.apple.com/download/all/?q=command%20line%20tools and install Command Line Tools 16.1 (not the latest one)"
+read -n 1 -r -s -p $'    Once you''re done, press any key to continue or Ctrl+C to abort...'
+echo ""
 
 # Install chezmoi
 command -v chezmoi >/dev/null 2>&1 || \
